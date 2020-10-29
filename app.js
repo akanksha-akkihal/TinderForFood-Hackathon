bab821d809b7a4cea0e16f7191a4e4ce6bd5fad1
Akanksha27 <akankshaakkihal27@gmail.com>
2020-10-30 01:14:42 +0530

    frontend sorted

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app= express()
+const port=5000
+
+app.set('view engine','ejs');
+app.listen(port);
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+  res.render('index');
+});
+
+app.get('/rest-page',(req,res)=>{
+  res.render('restPage');
+})
+
+app.get('/chat',(req,res)=>{
+  res.render('chatroom');
+})
+
+app.get('/bill',(req,res)=>{
+  res.render('billsplit');
+})
+
