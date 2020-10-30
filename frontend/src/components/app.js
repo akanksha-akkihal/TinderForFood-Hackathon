4c5f4daed9adf7dcecb5739baa644fbd118bf7fb
amankumarm <amankumarm441@gmail.com>
2020-10-30 12:07:29 +0530

    google maps integrated at /react

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,14 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component,useEffect,useState } from 'react'
 import ReactDom from 'react-dom'
 import Test from './components/test'
+import Gmap from './components/Gmap'
 
-class App extends Component {
-    render() {
+function App(){
+    const [position, setposition] = useState([0,0,0])
+
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((e)=>{
+                setposition([e.coords.latitude,e.coords.longitude,true])
+            })
+        }
+        else{
+            setposition([0,0,false])
+        }
+    }, [])
+
+
+    //zomato apis is to be done
+    
+
+
+
+    
         return (
-            <div>
-                <Test Greet="chetas"/>
-            </div>
-        )
-    }
-}
+            <>
+            <Gmap pos={position}/>
+            </>
+        )}
+
+            
+
+
 ReactDom.render(<App/>,document.getElementById("root"));
